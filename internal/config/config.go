c486a9ca14d6e0492dd0acacedf45e55b09a445d
DMITRIY <[dima.lewshin.98@mail.ru]>
2026-03-23 23:51:33 +0300

    firstinit

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
@@ -10,9 +11,15 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	var serverHostPort = flag.String("a", "localhost:8888", "server host:port")
-	var shortenURLHostPort = flag.String("b", "http://localhost:8000", "shorten url http://host:port")
+	serverHostPort := flag.String("a", "localhost:8888", "server host:port")
+	shortenURLHostPort := flag.String("b", "http://localhost:8000", "shorten url http://host:port")
 	flag.Parse()
+	if envServerHostPort := os.Getenv("SERVER_ADDRESS"); envServerHostPort != "" {
+		*serverHostPort = envServerHostPort
+	}
+	if envShortenURLHostPort := os.Getenv("BASE_URL"); envShortenURLHostPort != "" {
+		*shortenURLHostPort = envShortenURLHostPort
+	}
 	return &Config{
 		ServerHostPort:     *serverHostPort,
 		ShortenURLHostPort: *shortenURLHostPort,
