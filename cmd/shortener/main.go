0f846d4be965cbd2377ac8002db8e6bba5ac3ae2
DMITRIY <[dima.lewshin.98@mail.ru]>
2026-03-30 23:02:36 +0300

    firstinit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+
+	"go.uber.org/zap"
 
 	"github.com/dimalewshin98-glitch/ShortyURL/internal/config"
 	"github.com/dimalewshin98-glitch/ShortyURL/internal/handler"
+	"github.com/dimalewshin98-glitch/ShortyURL/internal/logger"
 	"github.com/dimalewshin98-glitch/ShortyURL/internal/repository"
 	"github.com/dimalewshin98-glitch/ShortyURL/internal/service"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	config := config.NewConfig()
+	cfg := config.NewConfig()
+	if err := logger.Initialize(cfg.LogLevel); err != nil {
+		panic(err)
+	}
 	repository := repository.NewInmemoryRepository()
-	shorterService := service.NewShorterService(repository, config)
+	shorterService := service.NewShorterService(repository, cfg)
 	requestsHandler := handler.NewRequestsHandler(shorterService)
 	r := chi.NewRouter()
-	r.Post("/", requestsHandler.Shorten)
-	r.Get("/{id}", requestsHandler.GetURL)
-	fmt.Println("server starting at: " + config.ServerHostPort)
-	log.Fatal(http.ListenAndServe(config.ServerHostPort, r))
+	r.Post("/", logger.RequestLogger(requestsHandler.Shorten))
+	r.Get("/{id}", logger.RequestLogger(requestsHandler.GetURL))
+	logger.Log.Info("Running server", zap.String("address", cfg.ServerHostPort))
+	err := http.ListenAndServe(cfg.ServerHostPort, r)
+	if err != nil {
+		logger.Log.Fatal("Server failed", zap.Error(err))
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,11 +8,13 @@ import (
 type Config struct {
 	ServerHostPort     string
 	ShortenURLHostPort string
+	LogLevel           string
 }
 
 func NewConfig() *Config {
 	serverHostPort := flag.String("a", "localhost:8888", "server host:port")
 	shortenURLHostPort := flag.String("b", "http://localhost:8000", "shorten url http://host:port")
+	logLevel := flag.String("l", "info", "log level")
 	flag.Parse()
 	if envServerHostPort := os.Getenv("SERVER_ADDRESS"); envServerHostPort != "" {
 		*serverHostPort = envServerHostPort
@@ -20,8 +22,12 @@ func NewConfig() *Config {
 	if envShortenURLHostPort := os.Getenv("BASE_URL"); envShortenURLHostPort != "" {
 		*shortenURLHostPort = envShortenURLHostPort
 	}
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		*logLevel = envLogLevel
+	}
 	return &Config{
 		ServerHostPort:     *serverHostPort,
 		ShortenURLHostPort: *shortenURLHostPort,
+		LogLevel:           *logLevel,
 	}
 }
